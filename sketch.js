54741566efd51ff77e7f6e417fd6f7a4a5749ae6
Benmcpherson427 <161518366+Benmcpherson427@users.noreply.github.com>
2024-05-10 11:30:50 -0600

    started to work on damage function

--- sketch.js
+++ sketch.js
@@ -16,6 +16,16 @@ function preload() {
   bg = loadImage("other displays/pokemonbackground.png");
 }
 
+class move {
+  constructor(type, damage, hitType) {
+    this.type = type;
+    this.damage = damage;
+    this.hitType = hitType;
+  }
+
+  
+}
+
 class pokemon {
   constructor(HP, ATK, DEF, SPA, SPD, SPE, type1, type2) {
     this.HP = HP;
@@ -27,29 +37,46 @@ class pokemon {
     this.type1 = type1;
     this.type2 = type2;
   }
+
+
 }
 
 
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  // charizard = new pokemon(78, 84, 78, 109, 85, 100, "fire", "flying");
+  let charizard = new pokemon(78, 84, 78, 109, 85, 100, "fire", "flying");
+  let pikachu = new pokemon(45, 80, 50, 75, 60, 120, "electric", "none");
 }
 
 function draw() {
   background(255);
-  image(bg, 0, 0, windowWidth, windowHeight);
+  image(bg, windowWidth/2, windowHeight/2, windowWidth, windowHeight);
   loadPokemon();
   optionsBox();
 }
 
 function optionsBox() {
-  fill(200);
-  rect(windowWidth/2, windowHeight*(3/4), windowWidth, windowHeight);
+  fill("red");
+  rect(windowWidth*3/4, windowHeight*(3/4), windowWidth, windowHeight);
 
+}
+
+function mousePressed() {
+  if (mouseX >= windowWidth*3/4 && mouseX <= windowWidth && mouseY >= windowHeight*(3/4) && mouseY <= windowHeight) {
+    fill(200);
+    rect(windowWidth/2, windowHeight*(3/4), windowWidth, windowHeight);
+  }
 }
 
 function loadPokemon() {
-  image(pikachuIMG, windowWidth*(4.5/7), windowHeight/4, pikachuIMG.width*4, pikachuIMG.height*4);
-  image(charizardIMG, windowWidth/7, windowHeight/2.5, charizardIMG.width *5, charizardIMG.height*5);
+  imageMode(CENTER);
+  image(pikachuIMG, windowWidth*(5.3/7), windowHeight/2, pikachuIMG.width*4, pikachuIMG.height*4);
+  image(charizardIMG, windowWidth/4.5, windowHeight - charizardIMG.height*2.2, charizardIMG.width *5, charizardIMG.height*5);
 }
+
+function dealDamage() {
+  
+}
+
+
