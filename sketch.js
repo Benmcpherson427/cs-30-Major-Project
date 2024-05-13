abc049447be49363105996cbba1ae3dd5a17cf2e
Benmcpherson427 <161518366+Benmcpherson427@users.noreply.github.com>
2024-05-13 14:11:11 -0600

    worked on damage, as well as got an idea on how to implement more than 2 pokemon

--- sketch.js
+++ sketch.js
@@ -9,6 +9,8 @@ let diameter = 100;
 let charizardIMG;
 let pikachuIMG;
 let bg;
+let pikachu;
+let charizard;
 
 function preload() {
   pikachuIMG = loadImage("front sprites/pikachufront.png");
@@ -17,17 +19,16 @@ function preload() {
 }
 
 class move {
-  constructor(type, damage, hitType) {
+  constructor(movedUsed, type, damage, hitType) {
+    this.movedUsed = movedUsed;
     this.type = type;
     this.damage = damage;
     this.hitType = hitType;
   }
-
-  
 }
 
 class pokemon {
-  constructor(HP, ATK, DEF, SPA, SPD, SPE, type1, type2) {
+  constructor(HP, ATK, DEF, SPA, SPD, SPE, type1, type2, level) {
     this.HP = HP;
     this.ATK = ATK;
     this.DEF = DEF;
@@ -36,6 +37,7 @@ class pokemon {
     this.SPE = SPE;
     this.type1 = type1;
     this.type2 = type2;
+    this.level = 50;
   }
 
 
@@ -45,29 +47,58 @@ class pokemon {
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  let charizard = new pokemon(78, 84, 78, 109, 85, 100, "fire", "flying");
-  let pikachu = new pokemon(45, 80, 50, 75, 60, 120, "electric", "none");
+  charizard = new pokemon(78, 84, 78, 109, 85, 100, "fire", "flying");
+  pikachu = new pokemon(45, 80, 50, 75, 60, 120, "electric", "none");
 }
 
 function draw() {
   background(255);
   image(bg, windowWidth/2, windowHeight/2, windowWidth, windowHeight);
   loadPokemon();
-  optionsBox();
+  healthbox();
+  // optionsBox();
+  
 }
 
-function optionsBox() {
-  fill("red");
-  rect(windowWidth*3/4, windowHeight*(3/4), windowWidth, windowHeight);
-
-}
-
-function mousePressed() {
-  if (mouseX >= windowWidth*3/4 && mouseX <= windowWidth && mouseY >= windowHeight*(3/4) && mouseY <= windowHeight) {
-    fill(200);
-    rect(windowWidth/2, windowHeight*(3/4), windowWidth, windowHeight);
+function keyPressed() {
+  crit = random(2);
+  STAB = true
+  if (key === "d") {
+    pikachu.HP -= ((((100/5) + 2)*this.damage*(charizard.ATK/pikachu.DEF)/50)+2)*
+  }
+  if (key === "a") {
+    charizard.HP -= 15;
   }
 }
+
+// function optionsBox() {
+//   fill("red");
+//   rect(windowWidth*3/4, windowHeight*(3/4), windowWidth, windowHeight);
+//   fill("black");
+//   textSize(60);
+//   textAlign(CENTER, CENTER);
+//   text("BATTLE", windowWidth*(7/8), windowHeight*(7/8));
+// }
+
+function healthbox() {
+  fill("black");
+  textSize(60);
+  textAlign(CENTER, CENTER);
+  text(pikachu.HP, windowWidth*(7/8), windowHeight/8);
+
+  fill("black");
+  textSize(60);
+  textAlign(CENTER, CENTER);
+  text(charizard.HP, windowWidth/8, windowHeight/8);
+}
+
+// function mousePressed() {
+//   console.log(mouseX, mouseY);
+//   if (mouseX >= 1200 && mouseX <= 1600 && mouseY >= 570 && mouseY <= 765) {
+//     fill(200);
+//     rect(windowWidth*3/4, windowHeight*(3/4), windowWidth, windowHeight);
+//   }
+// }
 
 function loadPokemon() {
   imageMode(CENTER);
@@ -79,4 +110,11 @@ function dealDamage() {
   
 }
 
-
+function kill() {
+  if (pikachu.HP <= 0) {
+    pikachu.HP = 0;
+  }
+  if (charizard.HP <= 0) {
+    charizard.HP = 0;
+  }
+}
